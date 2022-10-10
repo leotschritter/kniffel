e23cf8a3a6eeaac70e1bcb4fed2c895ed847f772
Leo Tschritter <leotschritter@gmail.com>
2022-10-10 19:32:45 +0200

    initial commit

--- kniffel/src/main/scala/Main.scala
+++ kniffel/src/main/scala/Main.scala
@@ -1,5 +1,18 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+val f_column: List[String] =
+  List("1", "2", "3", "4", "5", "6", "G", "B", "O", "3x", "4x", "FH", "KS", "GS", "KN", "CH", "U", "O", "E")
+val builder = List.newBuilder[String]
 
-def msg = "I was compiled by Scala 3. :)"
+def cells(cellWidth: Int = 3, cellNum: Int = 3, desc: String) = 
+  "|" + desc + ("|" + " " * cellWidth) * (cellNum - 1) + "|" + '\n'
+  
+def bar(cellWidth: Int = 3, cellNum: Int = 3) = (("+" + ("-") * cellWidth) * cellNum) + "+" + '\n'
+
+def mesh(cellWidth: Int = 3, cellNum: Int = 3):String = {
+  for(s:String <- f_column)
+    builder += (bar(cellWidth) + cells(cellWidth, cellNum,  s.padTo(3, ' ')))
+  builder += bar(cellWidth)
+  builder.toString.replace("ListBuffer(", "").replace(", ", "").replace(")", "")
+}
+
+@main def main: Unit = 
+  println(mesh())
