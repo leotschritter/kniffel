c30202facd4470ffac8bc1a9aefd0eba16c8e0c6
Leo Tschritter <leotschritter@gmail.com>
2022-10-14 12:40:41 +0200

    fixed methods so they return datatypes

--- kniffel/src/main/scala/Main.scala
+++ kniffel/src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 val f_column: List[String] =
   List("1", "2", "3", "4", "5", "6", "G", "B", "O", "3x", "4x", "FH", "KS", "GS", "KN", "CH", "U", "O", "E")
+
 val number_of_players: Int = 4
 
 def cells(cellWidth: Int = 3, desc: String) = "|" + desc + ("|" + " " * cellWidth) * (number_of_players) + "|" + '\n'
@@ -7,17 +8,13 @@ def cells(cellWidth: Int = 3, desc: String) = "|" + desc + ("|" + " " * cellWidt
 def bar(cellWidth: Int = 3) = (("+" + ("-") * cellWidth) * (number_of_players + 1)) + "+" + '\n'
 
 
-def header(cellWidth: Int = 3): Unit = {
-  print(" " * (cellWidth + 1))
-  (for (n <- List.range(1, number_of_players + 1)) print("|" + ("P" + n).padTo(3, ' ')))
-  println("|")
+def header(cellWidth: Int = 3): List[String] = {
+  (" " * (cellWidth + 1)) :: (for (n <- List.range(1, number_of_players + 1)) yield ("|" + ("P" + n).padTo(3, ' ')))
 }
 
-def mesh(cellWidth: Int = 3): Unit = {
-  header()
-  for (s <- f_column) print(bar(cellWidth) + cells(cellWidth, s.padTo(3, ' ')))
-  print(bar(cellWidth))
+def mesh(cellWidth: Int = 3): String = {
+  (header() :+ "\n" :+ (for (s <- f_column) yield (bar(cellWidth) + cells(cellWidth, s.padTo(3, ' ')))).mkString("") :+ (bar(cellWidth))).mkString("")
 }
 
 @main def main: Unit =
-  mesh()
+  println(mesh())
