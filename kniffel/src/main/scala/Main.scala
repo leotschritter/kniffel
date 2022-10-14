4c2e4e453a8dcfcf532c8e727c93f2a690253c9e
Leo Tschritter <leotschritter@gmail.com>
2022-10-14 09:58:28 +0200

    corrected solution

--- kniffel/src/main/scala/Main.scala
+++ kniffel/src/main/scala/Main.scala
@@ -1,18 +1,23 @@
 val f_column: List[String] =
   List("1", "2", "3", "4", "5", "6", "G", "B", "O", "3x", "4x", "FH", "KS", "GS", "KN", "CH", "U", "O", "E")
-val builder = List.newBuilder[String]
+val number_of_players: Int = 4
 
-def cells(cellWidth: Int = 3, cellNum: Int = 3, desc: String) = 
-  "|" + desc + ("|" + " " * cellWidth) * (cellNum - 1) + "|" + '\n'
-  
-def bar(cellWidth: Int = 3, cellNum: Int = 3) = (("+" + ("-") * cellWidth) * cellNum) + "+" + '\n'
+def cells(cellWidth: Int = 3, desc: String) = "|" + desc + ("|" + " " * cellWidth) * (number_of_players) + "|" + '\n'
 
-def mesh(cellWidth: Int = 3, cellNum: Int = 3):String = {
-  for(s:String <- f_column)
-    builder += (bar(cellWidth) + cells(cellWidth, cellNum,  s.padTo(3, ' ')))
-  builder += bar(cellWidth)
-  builder.toString.replace("ListBuffer(", "").replace(", ", "").replace(")", "")
+def bar(cellWidth: Int = 3) = (("+" + ("-") * cellWidth) * (number_of_players + 1)) + "+" + '\n'
+
+
+def header(cellWidth: Int = 3): Unit = {
+  print(" " * (cellWidth + 1))
+  (for (n <- List.range(1, number_of_players + 1)) print("|" + ("P" + n).padTo(3, ' ')))
+  println("|")
 }
 
-@main def main: Unit = 
-  println(mesh())
+def mesh(cellWidth: Int = 3): Unit = {
+  header()
+  for (s <- f_column) print(bar(cellWidth) + cells(cellWidth, s.padTo(3, ' ')))
+  print(bar(cellWidth))
+}
+
+@main def main: Unit =
+  mesh()
