e001163080098a50f3c07753bbf1f9ce63cc988a
Leo Tschritter <leotschritter@gmail.com>
2022-10-21 09:03:26 +0200

    changed project structure

--- kniffel/src/main/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
-  println("Test User 2 new")
-  println("Test 2 User 2")
-def msg = "I was compiled by Scala 3. :)"
-
-def coolMethod(c:Int, b:Int): Int = c * b
