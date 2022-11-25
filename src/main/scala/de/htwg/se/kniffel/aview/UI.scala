6fee87c6fe0e0ca4b390df9a28f95b67e0a5dbe2
Leo Tschritter <leotschritter@gmail.com>
2022-11-25 11:31:59 +0100

    changed template method

--- src/main/scala/de/htwg/se/kniffel/aview/UI.scala
+++ src/main/scala/de/htwg/se/kniffel/aview/UI.scala
@@ -1,7 +1,34 @@
-package de.htwg.se.kniffel.aview
+package de.htwg.se.kniffel
+package aview
 
-trait UI {
-  def run : Unit
-  def inputLoop() : Unit
+import controller.Controller
+import util.Observer
+import model.{Field, Game, Move, Player}
+import model.dicecup.DiceCup
 
+abstract class UI(controller: Controller) extends Observer {
+
+
+  def run(): Unit
+
+  def gameAndFieldInput(player: Player, move: Move, indexList: List[Int] = List(6, 7, 8, 16, 17, 18)): Unit = {
+    val currentPlayerIndex: Int = controller.game.playersList.indexOf(player)
+    val currentSumList: List[Int] = controller.game.getCurrentList
+    controller.doAndPublish(controller.putValToField, Move(move.value, currentPlayerIndex, move.y))
+    if (move.y < 6)
+      controller.doAndPublish(controller.sum, move.value.toInt + currentSumList.head, currentSumList(3))
+    else
+      controller.doAndPublish(controller.sum, currentSumList.head, move.value.toInt + currentSumList(3))
+    for (l <- indexList)
+      controller.doAndPublish(
+        controller.putValToField,
+        Move(controller.game.getCurrentList(indexList.indexOf(l)).toString, currentPlayerIndex, l)
+      )
+    controller.doAndPublish(controller.next().get)
+  }
+  def diceCupInput(): Unit = controller.doAndPublish(controller.nextRound())
+
+  def diceCupPutIn(pi: List[Int]): Unit = controller.doAndPublish(controller.putIn, pi)
+
+  def diceCupPutOut(po: List[Int]): Unit = controller.doAndPublish(controller.putOut, po)
 }
