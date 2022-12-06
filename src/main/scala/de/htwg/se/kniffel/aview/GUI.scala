146a1e5d2271716f72f8c25ced49f7054e275b55
Leo Tschritter <leotschritter@gmail.com>
2022-12-06 18:42:38 +0100

    changed GUI

--- src/main/scala/de/htwg/se/kniffel/aview/GUI.scala
+++ src/main/scala/de/htwg/se/kniffel/aview/GUI.scala
@@ -41,11 +41,20 @@ class GUI(controller: Controller) extends Frame, UI(controller) :
   val imgToInt: Map[String, Int] = Map(diceLinks.head -> 1, diceLinks(1) -> 2, diceLinks(2) -> 3,
     diceLinks(3) -> 4, diceLinks(4) -> 5, diceLinks.last -> 6)
 
-  val pnl_center = new RightCellPanel(controller.field.defaultPlayers)
+  enum stateOfDices:
+    case initial
+    case running
 
   def update(e: Event): Unit = e match
     case Event.Quit => this.dispose()
-    case Event.Move => pnl_center.repaint(); repaint()
+    case Event.Move =>
+      contents = new BorderPanel {
+        add(new Label("Welcome to Kniffel"), BorderPanel.Position.North)
+        add(new LeftCellPanel(), BorderPanel.Position.West)
+        add(new CenterCellPanel(), BorderPanel.Position.Center)
+        add(new RightPanel(stateOfDices.running), BorderPanel.Position.East)
+      }
+      repaint()
 
   menuBar = new MenuBar {
     contents += new Menu("File") {
@@ -56,11 +65,9 @@ class GUI(controller: Controller) extends Frame, UI(controller) :
   }
   contents = new BorderPanel {
     add(new Label("Welcome to Kniffel"), BorderPanel.Position.North)
-    val pnl_left = new LeftCellPanel()
-    // val pnl_center = new RightCellPanel(controller.field.defaultPlayers)
-    add(pnl_left, BorderPanel.Position.West)
-    add(pnl_center, BorderPanel.Position.Center)
-    add(new RightPanel(), BorderPanel.Position.East)
+    add(new LeftCellPanel(), BorderPanel.Position.West)
+    add(new CenterCellPanel(), BorderPanel.Position.Center)
+    add(new RightPanel(stateOfDices.initial), BorderPanel.Position.East)
   }
   pack()
   centerOnScreen()
@@ -76,23 +83,13 @@ class GUI(controller: Controller) extends Frame, UI(controller) :
       border = Swing.LineBorder(new Color(0, 0, 0))
     }).toList
 
-  /*for (i <- 0 until 19) {
-    for (j <- 0 until numberOfPlayers) {
-      // contents += new CellButton("" + j + ", " + i, j, i)
-      contents += new TextArea {
-        text = controller.field.matrix.cell(j, i)
-        preferredSize = new Dimension(60, 20)
-        border = Swing.LineBorder(new Color(0, 0, 0))
-      }
-    }
-  }*/
-  //lass LeftCellPanel() extends GridPanel(1, 2) :
-  /*class CellPanel(numberOfPlayers: Int) extends GridPanel(19, numberOfPlayers):
-    List()*/
-  /*class BorderCellPanel(numberOfPlayers: Int) extends BorderPanel(1, numberOfPlayers):
-    contents += new LeftCellPanel()
-    contents += new RightCellPanel(numberOfPlayers)*/
-  def updateDiceCup(lstViewLeft: ListView[ImageIcon], lstViewRight: ListView[ImageIcon], lbl_rem: Label, btn_dice: Button = new Button()): Unit = {
+  def getXIndex: Int = controller.game.currentPlayer.playerID
+
+  def isEmpty(y: Int): Boolean = controller.field.matrix.isEmpty(getXIndex, y)
+
+  def disableList: List[Int] = (for {y <- 0 until 19 if !isEmpty(y)} yield y).toList
+
+/*  def updateDiceCup(lstViewLeft: ListView[ImageIcon], lstViewRight: ListView[ImageIcon], lbl_rem: Label, btn_dice: Button = new Button()): Unit = {
     if (controller.diceCup.remDices == -1)
       btn_dice.enabled = false
     val lstLeft: List[ImageIcon] = for (s <- controller.diceCup.inCup) yield intToImg(s)
@@ -100,81 +97,94 @@ class GUI(controller: Controller) extends Frame, UI(controller) :
     lbl_rem.text = "<html>Verbleibende<br>Würfe: " + (controller.diceCup.remDices + 1)
     lstViewLeft.listData = lstLeft
     lstViewRight.listData = lstRight
-  }
+  }*/
 
-  class RightPanel() extends BorderPanel :
+  class RightPanel(state: stateOfDices, inCup: List[Int] = controller.diceCup.inCup,
+                   locked: List[Int] = controller.diceCup.locked, remaining_moves: Int = controller.diceCup.remDices) extends BorderPanel :
     val lstViewLeft: ListView[ImageIcon] = new ListView[ImageIcon]() {
       selection.intervalMode = IntervalMode.MultiInterval
+      if(state.==(stateOfDices.running))
+        listData = for (s <- controller.diceCup.inCup) yield intToImg(s)
       preferredSize = new Dimension(100, 500)
     }
     val lstViewRight: ListView[ImageIcon] = new ListView[ImageIcon]() {
       selection.intervalMode = IntervalMode.MultiInterval
+      listData = for (s <- controller.diceCup.locked) yield intToImg(s)
       preferredSize = new Dimension(100, 500)
     }
-    val lbl_rem: Label = new Label("<html>Verbleibende<br>Würfe: 3")
-    add(new TopInnerPanel(lbl_rem), BorderPanel.Position.North)
+    add(new TopInnerPanel(), BorderPanel.Position.North)
     add(lstViewLeft, BorderPanel.Position.West)
-    add(new RightInnerPanel(lstViewLeft, lstViewRight, lbl_rem), BorderPanel.Position.Center)
+    add(new RightInnerPanel(), BorderPanel.Position.Center)
     add(lstViewRight, BorderPanel.Position.East)
+    add(new BottomPanel(), BorderPanel.Position.South)
 
-  class TopInnerPanel(lbl_rem: Label) extends GridPanel(1, 3) :
-    contents += new Label("Im Becher")
-    contents += lbl_rem
-    contents += new Label("Rausgenommen")
+    class BottomPanel() extends FlowPanel:
+      contents += new Label(controller.game.currentPlayer.playerName + " ist an der Reihe.")
 
-  class RightInnerPanel(lstViewLeft: ListView[ImageIcon], lstViewRight: ListView[ImageIcon], lbl_rem: Label) extends BoxPanel(Orientation.Vertical) {
-    val buttonDimension: Dimension = new Dimension(90, 50)
-    contents += new Button {
-      icon = new ImageIcon("src/main/resources/right_arrow.png") {
-        preferredSize = buttonDimension
-      }
-      preferredSize = buttonDimension
-      listenTo(mouse.clicks)
-      reactions += {
-        case MouseClicked(src, pt, mod, clicks, props) =>
-          val intList: List[Int] = for (s <- lstViewLeft.selection.items.toList) yield imgToInt(s.toString)
-          diceCupPutOut(intList)
-          updateDiceCup(lstViewLeft, lstViewRight, lbl_rem)
-      }
-    }
-    contents += new Button {
-      icon = new ImageIcon("src/main/resources/left_arrow.png") {
-        preferredSize = buttonDimension
-      }
-      preferredSize = buttonDimension
-      listenTo(mouse.clicks)
-      reactions += {
-        case MouseClicked(src, pt, mod, clicks, props) =>
-          val intList: List[Int] = for (s <- lstViewRight.selection.items.toList) yield imgToInt(s.toString)
-          diceCupPutIn(intList)
-          updateDiceCup(lstViewLeft, lstViewRight, lbl_rem)
-      }
-    }
-    val btn_dice: Button = new Button {
-      icon = new ImageIcon("src/main/resources/flying_dices_small.png") {
-        preferredSize = buttonDimension
-      }
-      preferredSize = buttonDimension
-      listenTo(mouse.clicks)
-      reactions += {
-        case MouseClicked(src, pt, mod, clicks, props) =>
-          controller.doAndPublish(controller.dice())
-          updateDiceCup(lstViewLeft, lstViewRight, lbl_rem, btn_dice)
-      }
-    }
-    contents += btn_dice
-  }
+    class TopInnerPanel() extends GridPanel(1, 3) :
+      contents += new Label("Im Becher")
+      contents += new Label("<html>Verbleibende<br>Würfe: " + (remaining_moves + 1))
+      contents += new Label("Rausgenommen")
 
-  //contents += new Bu
+    class RightInnerPanel() extends BoxPanel(Orientation.Vertical) {
+      val buttonDimension: Dimension = new Dimension(90, 50)
+      contents += new Button {
+        icon = new ImageIcon("src/main/resources/right_arrow.png") {
+          preferredSize = buttonDimension
+        }
+        preferredSize = buttonDimension
+        listenTo(mouse.clicks)
+        if(remaining_moves != 2)
+          enabled = true
+          reactions += {
+          case MouseClicked(src, pt, mod, clicks, props) =>
+            val intList: List[Int] = for (s <- lstViewLeft.selection.items.toList) yield imgToInt(s.toString)
+            diceCupPutOut(intList)
+          }
+        else
+          enabled = false
+      }
+      contents += new Button {
+        icon = new ImageIcon("src/main/resources/left_arrow.png") {
+          preferredSize = buttonDimension
+        }
+        preferredSize = buttonDimension
+        listenTo(mouse.clicks)
+        if(remaining_moves != 2)
+          enabled = true
+          reactions += {
+            case MouseClicked(src, pt, mod, clicks, props) =>
+              val intList: List[Int] = for (s <- lstViewRight.selection.items.toList) yield imgToInt(s.toString)
+              diceCupPutIn(intList)
+          }
+        else
+          enabled = false
+      }
+      val btn_dice: Button = new Button {
+        icon = new ImageIcon("src/main/resources/flying_dices_small.png") {
+          preferredSize = buttonDimension
+        }
+        preferredSize = buttonDimension
+        listenTo(mouse.clicks)
+        if(remaining_moves >= 0)
+          reactions += {
+            case MouseClicked(src, pt, mod, clicks, props) =>
+              controller.doAndPublish(controller.dice())
+          }
+        else
+          enabled = false
+      }
+      contents += btn_dice
+    }
+
   class LeftCellPanel() extends GridPanel(1, 2) :
-    contents += new LeftCellPanelFirstColumn(field())
+    contents += new LeftCellPanelFirstColumn()
     contents += new LeftCellPanelSecondColumn()
 
-  class LeftCellPanelFirstColumn(var lst_label: List[Label]) extends GridPanel(19, 1) :
+  class LeftCellPanelFirstColumn(disableList: List[Int] = disableList) extends GridPanel(19, 1) :
     for (i <- 0 until 6) {
-      contents += new CellButton("", i, 0, lst_label) {
+      contents += new CellButton("", i, disableList.contains(i)) {
         icon = new ImageIcon(diceLinksField(i))
-        //size = new Dimension(12, 12)
       }
     }
     for (i <- 0 until 13) {
@@ -187,7 +197,7 @@ class GUI(controller: Controller) extends Frame, UI(controller) :
           border = Swing.LineBorder(new Color(0, 0, 0))
         }
       else
-        contents += new CellButton("", i + 6, 0, lst_label) {
+        contents += new CellButton("", i + 6, disableList.contains(i + 6)) {
           font = new Font("Arial", 0, 13)
           horizontalAlignment = Alignment.Center
           text = first_column_second_part(i)
@@ -198,7 +208,7 @@ class GUI(controller: Controller) extends Frame, UI(controller) :
 
   class LeftCellPanelSecondColumn() extends GridPanel(19, 1) :
     for (i <- 0 until 19) {
-      if(i == 6 || i == 8 || 15 < i && i < 19)
+      if (i == 6 || i == 8 || 15 < i && i < 19)
         contents += new Label {
           icon = new ImageIcon("src/main/resources/right_arrow.png")
           border = Swing.LineBorder(new Color(0, 0, 0))
@@ -212,43 +222,27 @@ class GUI(controller: Controller) extends Frame, UI(controller) :
         }
     }
 
-  class RightCellPanel(numberOfPlayers: Int = controller.game.playersList.length) extends GridPanel(19, numberOfPlayers) :
-    //List() :: (for(j <- 0 until 19) do (for i <- 0 until numberOfPlayers) (contents+= new CellButton("", i, j)))
+  class CenterCellPanel(numberOfPlayers: Int = controller.field.defaultPlayers) extends GridPanel(19, numberOfPlayers):
     for (x <- field(numberOfPlayers)) yield contents += x
-  /*for (i <- 0 until 19) {
-      for (j <- 0 until numberOfPlayers) {
-        // contents += new CellButton("" + j + ", " + i, j, i)
-        contents += new TextArea {
-          text = controller.field.matrix.cell(j, i)
-          preferredSize = new Dimension(60, 20)
-          border = Swing.LineBorder(new Color(0, 0, 0))
-        }
+
+
+  class CellButton(value: String, y: Int, isDisabled: Boolean = true) extends Button(value) :
+    if (!isDisabled)
+      listenTo(mouse.clicks)
+      reactions += {
+        case MouseClicked(src, pt, mod, clicks, props)
+        =>
+          if isEmpty(y)
+          then
+            writeDown(Move(getValue, getXIndex, y))
+            update(Event.Move)
+          else
+            errorMessage()
+            None
       }
-    }*/
-
-
-  // def button(value: String) = new Button(value)
-
-
-  class CellButton(value: String, x: Int, y: Int, var lst_label: List[Label]) extends Button(value) :
-
-    listenTo(mouse.clicks)
-
-    reactions += {
-      case MouseClicked(src, pt, mod, clicks, props)
-      =>
-        if isEmpty(getYIndex, x)
-        then
-          writeDown(Move(getValue, getYIndex, x))
-          update(Event.Move)
-          lst_label = field()
-        else errorMessage(); None
-    }
-
-    def getYIndex: Int = controller.game.currentPlayer.playerID
-
-    def isEmpty(x: Int, y: Int): Boolean = controller.field.matrix.isEmpty(x, y)
+    else
+      enabled = false
 
     def errorMessage(): Unit = Dialog.showMessage(contents.head, "Feld ist schon belegt!", title = "Falsche Eingabe", messageType = Dialog.Message.Error)
 
-    def getValue: String = controller.diceCup.getResult(x).toString
+    def getValue: String = controller.diceCup.getResult(y).toString
